f6bc00d3d692dedfd03db949a290bf21e61ffcea
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-03-07 16:11:03 +0100

    unnecessary comments are removed

--- api/src/index.js
+++ api/src/index.js
@@ -11,51 +11,6 @@ app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// // Future Meals
-// apiRouter.get("/future-meals", async (req, res) => {
-//   try {
-//     const futureMealsQuery =
-//       process.env.DB_CLIENT === "mysql2"
-//         ? "SELECT * FROM meal WHERE `when` > NOW() ;"
-//         : "SHOW TABLES;";
-//     const tables = await knex.raw(futureMealsQuery);
-//     res.json({ tables });
-//   } catch (error) {
-//     console.error("DB query failed:", error.message);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// // Past Meals
-// apiRouter.get("/past-meals", async (req, res) => {
-//   try {
-//     const pastMealsQuery =
-//       process.env.DB_CLIENT === "mysql2"
-//         ? "SELECT * FROM meal WHERE `when` < NOW() ;"
-//         : "SHOW TABLES;";
-//     const tables = await knex.raw(pastMealsQuery);
-//     res.json({ tables });
-//   } catch (error) {
-//     console.error("DB query failed:", error.message);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// // All Meals
-// apiRouter.get("/all-meals", async (req, res) => {
-//   try {
-//     const allMealsQuery =
-//       process.env.DB_CLIENT === "mysql2"
-//         ? "SELECT * FROM meal;"
-//         : "SHOW TABLES;";
-//     const tables = await knex.raw(allMealsQuery);
-//     res.json({ tables });
-//   } catch (error) {
-//     console.error("DB query failed:", error.message);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
 // This nested router example can also be replaced with your own sub-router
 apiRouter.use("/meals", mealsRouter);
 
