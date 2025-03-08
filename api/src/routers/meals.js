bc780017a35f89d9a3e953c9a116f82ddb4dac33
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-03-08 14:59:37 +0100

    Post, adding a new meal to database task completed

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -16,4 +16,16 @@ mealsRouter.get("/", async (req, res) => {
   }
 });
 
+// POST New Meal
+mealsRouter.post("/", async (req, res) => {
+  const newMeal = req.body;
+  try {
+    const mealID = await knex("meal").insert(newMeal);
+    console.log(mealID);
+    res.status(201).json({ message: "Successfully inserted new meal" });
+  } catch (error) {
+    console.error("DB query failed:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 export default mealsRouter;
