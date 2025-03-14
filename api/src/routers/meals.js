9ad321ee190a0ffb3c081f64f20a46d82d97c462
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-03-14 23:14:00 +0100

    dateAfter, dateBefore, limit, sortKey and sortDir query parameters added

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -6,66 +6,135 @@ const mealsRouter = express.Router();
 
 // GET All Meals
 mealsRouter.get("/", async (req, res) => {
-  let query = knex("meal").select([
-    "meal.id",
-    "meal.title",
-    "meal.description",
-    "meal.location",
-    "meal.when",
-    "meal.max_reservations",
-    "meal.price",
-    "meal.created_date",
-  ]);
+  try {
+    let query = knex("meal").select([
+      "meal.id",
+      "meal.title",
+      "meal.description",
+      "meal.location",
+      "meal.when",
+      "meal.max_reservations",
+      "meal.price",
+      "meal.created_date",
+    ]);
 
-  //maxPrice query parameter
-  if (req.query.maxPrice) {
-    const maxPrice = parseFloat(req.query.maxPrice);
-    if (!isNaN(maxPrice)) {
-      query.where("price", "<=", maxPrice);
+    // maxPrice query parameter
+    if (req.query.maxPrice) {
+      const maxPrice = parseFloat(req.query.maxPrice);
+      if (!isNaN(maxPrice)) {
+        query.where("price", "<=", maxPrice);
+      }
     }
-  }
-  // availableReservations query parameter
-  if (req.query.availableReservations) {
-    const available = req.query.availableReservations === "true";
-    query
-      .leftJoin("reservation", "meal.id", "reservation.meal_id")
-      .count("reservation.id as total_reservations")
-      .groupBy([
-        "meal.id",
-        "meal.title",
-        "meal.description",
-        "meal.location",
-        "meal.when",
-        "meal.max_reservations",
-        "meal.price",
-        "meal.created_date",
-      ])
-      .having(
-        "max_reservations",
-        available ? ">" : "<=",
-        knex.raw("COALESCE(COUNT(reservation.id), 0)")
-      );
+
+    // availableReservations query parameter
+    if (req.query.availableReservations) {
+      const available = req.query.availableReservations === "true";
+      query
+        .leftJoin("reservation", "meal.id", "reservation.meal_id")
+        .count("reservation.id as total_reservations")
+        .groupBy([
+          "meal.id",
+          "meal.title",
+          "meal.description",
+          "meal.location",
+          "meal.when",
+          "meal.max_reservations",
+          "meal.price",
+          "meal.created_date",
+        ])
+        .having(
+          "max_reservations",
+          available ? ">" : "<=",
+          knex.raw("COALESCE(COUNT(reservation.id), 0)")
+        );
+    }
+
+    // title query parameter
+    if (req.query.title) {
+      let title = req.query.title;
+      if (typeof title === "object" && title !== null) {
+        title = title.title;
+      }
+      title = String(title);
+      console.log("Extracted Title:", title);
+      query.whereILike("meal.title", `%${title}%`);
+    }
+
+    // dateAfter query parameter
+    if (req.query.dateAfter) {
+      let dateAfter = req.query.dateAfter;
+      const parsedDate = new Date(dateAfter);
+      if (isNaN(parsedDate.getTime())) {
+        return res
+          .status(400)
+          .json({ error: "Invalid dateAfter format. Use YYYY-MM-DD." });
+      }
+
+      const formattedDate = parsedDate
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+
+      query.where("when", ">=", formattedDate);
+    }
+
+    // dateBefore query parameter
+    if (req.query.dateBefore) {
+      let dateBefore = req.query.dateBefore;
+      const parsedDate = new Date(dateBefore);
+      if (isNaN(parsedDate.getTime())) {
+        return res
+          .status(400)
+          .json({ error: "Invalid dateBefore format. Use YYYY-MM-DD." });
+      }
+
+      const formattedDate = parsedDate
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+      query.where("when", "<=", formattedDate);
+    }
+
+    // sortKey and sortDir query parameters
+    if (req.query.sortKey) {
+      let sortKey = String(req.query.sortKey).toLowerCase();
+      const validKeys = ["when", "max_reservations", "price"];
+
+      if (validKeys.includes(sortKey)) {
+        let sortDir = "asc";
+
+        if (req.query.sortDir) {
+          const validDir = ["asc", "desc"];
+          let sortDirInput = String(req.query.sortDir).toLowerCase();
+          if (validDir.includes(sortDirInput)) {
+            sortDir = sortDirInput;
+          } else {
+            return res.status(400).json({ error: "Invalid direction" });
+          }
+        }
+
+        query.orderBy(sortKey, sortDir);
+      } else {
+        return res.status(400).json({ error: "Invalid sort key" });
+      }
+    }
+
+    let meals = await query;
+
+    // limit query parameter
+    if (req.query.limit) {
+      const limit = parseInt(req.query.limit);
+      if (!isNaN(limit) && limit > 0) {
+        meals = meals.slice(0, limit);
+      }
+    }
+
+    return res.status(200).json({ totalMeals: meals.length, meals });
+  } catch (error) {
+    console.error("DB query failed", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-// title query parameter
-if (req.query.title) {
-  let title = req.query.title;
-  if (typeof title === "object" && title !== null) {
-    title = title.title;
-  }
-  title = String(title);
-  console.log("Extracted Title:", title);
-  query.whereILike("meal.title", `%${title}%`);
-}
-try {
-  const meals = await query;
-  console.log(meals);
-  res.status(200).json({ meals });
-} catch (error) {
-  console.error("DB query failed:", error.message);
-  res.status(500).json({ error: "Internal Server Error" });
-}
 
 // POST New Meal
 mealsRouter.post("/", async (req, res) => {
