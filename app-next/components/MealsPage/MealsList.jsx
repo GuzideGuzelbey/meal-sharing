75f83077b639a5096270b76becf2d8d18c28ff03
GuzideGuzelbey <guzide.guzelbey@gmail.com>
2025-06-11 22:16:57 +0200

    Move Mealslist and Meal conponents to its own folder

---
+++ app-next/components/MealsPage/MealsList.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import Meal from "./Meal";
+import styles from "./meals.module.css";
+
+export default function MealsList({ limit }) {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3001/api/meals");
+      const data = await response.json();
+      setMeals(data.meals);
+    };
+    fetchData([]);
+  }, []);
+
+  const displayedMeals = limit ? meals.slice(0, limit) : meals;
+
+  return (
+    <div className={styles.mealsGrid}>
+      {displayedMeals.map((meal) => (
+        <Meal key={meal.id} meal={meal} />
+      ))}
+    </div>
+  );
+}
